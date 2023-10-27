a8b1587135023d16243d2ffb9fec624de6d91978
Tresor-ilunga <19im065@esisalama.org>
2023-10-27 17:18:35 +0200

    Initial commit

---
+++ components/ui/ToggleTheme.vue
@@ -0,0 +1,17 @@
+<template>
+    <button @click="toggleTheme()" aria-label="switch theme" border="~ gray-1 dark:gray-8" outline-none flex relative
+        text-gray-7 dark-text-gray-3 rd-full p-2 lg:p-3 bg-transparent>
+        <span i-carbon-moon text-xl bg="gray-7 dark:gray-3" hidden dark-flex></span>
+        <span i-carbon-sun text-xl bg="gray-7 dark:gray-3" flex dark-hidden></span>
+    </button>
+</template>
+<script setup lang="ts">
+const colorMode = useColorMode()
+const toggleTheme = () => {
+    if (colorMode.preference === "dark") {
+        colorMode.preference = "light"
+    } else {
+        colorMode.preference = "dark"
+    }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+    <main class="flex-1">
+        <SectionsHeroSection/>
+        <SectionsSocialProuf/>
+        <SectionsLogoCloud/>
+        <SectionsServices/>
+        <SectionsAboutContent/>
+        <SectionsCta/>
+    </main>
+</template>
+
+<script lang="ts" setup>
+useHead({
+  title: 'Agence X Template built with UnifyUI',
+  meta: [
+    {
+      name: 'description',
+      content: 'A sample landing page built using UnoCSS + NuxtJS and Unify UI blocks',
+    },
+  ],
+  link:[
+    {
+        href:"https://fonts.googleapis.com/css2?family=DM+Sans:wght@300;400;500;600;700&display=swap",
+        rel:"stylesheet"
+    }
+  ],
+})
+</script>
